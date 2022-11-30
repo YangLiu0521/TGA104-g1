ef6c178558d8ffbb9c092087220ab4e0a03b5e64
Abu <qxes1125@gmail.com>
2022-11-30 14:56:21 +0800

    add Abu.java

--- src/main/java/com/tibame/tga104/Hello.java
+++ src/main/java/com/tibame/tga104/Hello.java
@@ -17,6 +17,7 @@ public class Hello extends HttpServlet {
 			HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
 		out.println("Hello from Servlet");
+		System.out.println("Abus");
 	}
 	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
