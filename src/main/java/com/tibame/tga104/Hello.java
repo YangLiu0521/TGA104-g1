247d316e4f699f3356a705a8663a0fd8e9872d04
marc <ami760212@gmail.com>
2022-11-30 14:55:13 +0800

    marc update

--- src/main/java/com/tibame/tga104/Hello.java
+++ src/main/java/com/tibame/tga104/Hello.java
@@ -16,7 +16,7 @@ public class Hello extends HttpServlet {
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
-		out.println("Hello from Servlet");
+		out.println("everybody hands up");
 	}
 	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
